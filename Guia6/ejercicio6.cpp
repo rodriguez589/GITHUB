cec12f9dc3b647cb60e9988d911cfc543985cd30
rodriguez589 <62669121+rodriguez589@users.noreply.github.com>
2020-06-24 22:06:38 -0600

    Update ejercicio6.cpp

--- Guia6/ejercicio6.cpp
+++ Guia6/ejercicio6.cpp
@@ -1,37 +1,59 @@
-#include <iostream>
+#include "iostream"
+
 using namespace std;
 
-int contadorderepeticiones(int array[], int m, int n)
+void funcionx (int numero [], int t, int n) 
 {
-    int conteo = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-      if(array[i] == m)
-      {
-        conteo += 1;
+  
+  int repetir = 0;
+  int numeroarreglo = 0;
+  for (int i = 0; i < t; i++)
+  {
+    
+    if (n == numero[i])
+     {
+       repetir++;
+       numeroarreglo = numero[i];
       }
-    }
+  }
 
-    return n;
+   cout <<"el numero" << numeroarreglo << "se repite" << " " << repetir << endl;
 }
 
+void funciony (int numero[], int t, int n)
+{
+  for(int i = 0; i < t; i++)
+   {
+     if (n == numero[i])
+     {
+       numero[i] = 0;
+      }
+        
+   }
+}
 
 int main()
 {
-    int n,num;
-    int m;
-    cout << "Bienvenid@" << endl << endl;
-    cout << "Ingresar su numero porfavor:" << endl;
-    cin >> m;      
-    cout << "Ingresar el tama"<<char((164))<<"o del arreglo:" <<endl;
-    cin >> n;
-    cout << "ingrese los numeros que se encuentran dentro del array"<<endl;
-    cin>> num;
-    int array[n];
+  int numero[100], x, numerodeveces=0;
+  cout << endl;
 
-    int resultado = contadorderepeticiones(array, m, n);
-
-    cout << "la cantidad de repeticones del numero fueron: " <<m << endl; 
-    cout << "En el arreglo es:" << resultado << endl;
-}
+  cout <<"Veces que se repite un numero de dicho arreglo" << endl;
+  cout << "ingresar el numero de tama"<<char(164)<<"o del arreglo" << endl;
+  cin >> x;
+  for (int i= 0; i<x; i++)
+  {
+    cout << endl;
+    cout << i+1 << "digite un numero" << endl;
+    cin >> numero[i];
+  }
+  for(int i = 0; i<x; i++)
+  {
+    int n=numero[i];
+     if(n != 0)
+    {
+      funcionx (numero, x, n);
+      funciony (numero, x, n);
+    }
+  }
+  return 0;
+ }
